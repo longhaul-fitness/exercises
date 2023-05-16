e85ad5c52a92fa76f0213cf8a4af786a4fd45495
Bill Amberg <wamberg@longhaul.fitness>
2023-05-16 16:27:07 -0400

    --wip-- [skip ci]

--- vite.config.js
+++ vite.config.js
@@ -3,64 +3,64 @@ import { SvelteKitPWA } from '@vite-pwa/sveltekit';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-	plugins: [
-		sveltekit(),
-		SvelteKitPWA({
-			srcDir: './src',
-			mode: 'development',
-			strategies: 'generateSW',
-			scope: '/',
-			base: '/',
-			selfDestroying: process.env.SELF_DESTROYING_SW === 'true',
-			manifest: {
-				name: 'Long Haul Fitness',
-				short_name: 'LHF',
-				description: 'Free and open source list of fitness exercises',
-				start_url: '/',
-				scope: '/',
-				display: 'standalone',
-				theme_color: '#1c4b82',
-				icons: [
-					{
-						src: '/pwa-192x192.png',
-						sizes: '192x192',
-						type: 'image/png'
-					},
-					{
-						src: '/pwa-512x512.png',
-						sizes: '512x512',
-						type: 'image/png'
-					},
-					{
-						src: '/pwa-512x512.png',
-						sizes: '512x512',
-						type: 'image/png',
-						purpose: 'any maskable'
-					}
-				]
-			},
-			injectManifest: {
-				globPatterns: ['client/**/*.{js,css,ico,png,svg,webp,woff,woff2}']
-			},
-			workbox: {
-				globPatterns: ['client/**/*.{js,css,ico,png,svg,webp,woff,woff2}']
-			},
-			devOptions: {
-				enabled: true,
-				type: 'module',
-				navigateFallback: '/'
-			},
-			// if you have shared info in svelte config file put in a separate module and use it also here
-			kit: {}
-		})
-	],
-	server: {
-		fs: {
-			// Allow serving files from one level up to the project root
-			allow: ['..']
-		}
-	},
-	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
-	}
+  plugins: [
+    sveltekit(),
+    SvelteKitPWA({
+      srcDir: './src',
+      mode: 'development',
+      strategies: 'generateSW',
+      scope: '/',
+      selfDestroying: process.env.SELF_DESTROYING_SW === 'true',
+      manifest: {
+        name: 'Long Haul Fitness',
+        short_name: 'LHF',
+        description: 'Free and open source list of fitness exercises',
+        start_url: '/',
+        scope: '/',
+        base: process.env.NODE_ENV === 'production' ? '/exercises' : '',
+        display: 'standalone',
+        theme_color: '#1c4b82',
+        icons: [
+          {
+            src: '/pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: '/pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ]
+      },
+      injectManifest: {
+        globPatterns: ['client/**/*.{js,css,ico,png,svg,webp,woff,woff2}']
+      },
+      workbox: {
+        globPatterns: ['client/**/*.{js,css,ico,png,svg,webp,woff,woff2}']
+      },
+      devOptions: {
+        enabled: true,
+        type: 'module',
+        navigateFallback: '/'
+      },
+      // if you have shared info in svelte config file put in a separate module and use it also here
+      kit: {}
+    })
+  ],
+  server: {
+    fs: {
+      // Allow serving files from one level up to the project root
+      allow: ['..']
+    }
+  },
+  test: {
+    include: ['src/**/*.{test,spec}.{js,ts}']
+  }
 });
