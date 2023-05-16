cca1d913ab165293e51511cc518fc8c65f25d96b
Bill Amberg <wamberg@longhaul.fitness>
2023-05-16 10:18:59 -0400

    Show primary muscles as li

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,19 @@
 <script>
 	import { base } from '$app/paths';
 	export let data;
+
+	function translateMuscleIdsToNames(muscleIds) {
+		const muscleNames = muscleIds.map((muscleId) => data.muscles[muscleId]);
+		return muscleNames.sort();
+	}
 </script>
 
 <h1>Long Haul Fitness Exercise Directory</h1>
 <table>
 	<thead>
 		<tr>
-			<td>Exercise</td>
-			<td>Primary Muscles</td>
+			<th>Exercise</th>
+			<th>Primary Muscles</th>
 		</tr>
 	</thead>
 	<tbody>
@@ -16,9 +21,19 @@
 			<tr>
 				<td><a href="{base}/exercises/{exercise.pk}">{exercise.fields.name}</a></td>
 				<td>
-					{exercise.fields.primary_muscles.map((muscleId) => data.muscles[muscleId]).join(', ')}
+					<ul>
+						{#each translateMuscleIdsToNames(exercise.fields.primary_muscles) as muscle}
+							<li>{muscle}</li>
+						{/each}
+					</ul>
 				</td>
 			</tr>
 		{/each}
 	</tbody>
 </table>
+
+<style>
+	tbody tr:nth-child(even) {
+		background-color: #f6f8fa;
+	}
+</style>
