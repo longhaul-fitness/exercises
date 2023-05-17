46d7cd280727eb7ddc1292cdecfadc5a1f7a2dff
Bill Amberg <wamberg@longhaul.fitness>
2023-05-17 14:10:40 -0400

    Debounce and transition query input

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,20 @@
 <script>
 	import { base } from '$app/paths';
+	import { fade, fly } from 'svelte/transition';
 	import { translateMuscleIdsToNames } from '$lib/services/ui';
+
 	export let data;
 	const muscleMap = data.muscles;
 	let query = '';
+	let timer;
+
+	const debounce = (v) => {
+		clearTimeout(timer);
+		timer = setTimeout(() => {
+			query = v;
+		}, 200);
+	};
+
 	$: filteredExercises = data.exercises
 		.filter((e) => {
 			return e.fields.name.toLowerCase().match(query);
@@ -29,9 +40,17 @@
 </svelte:head>
 
 <h1>Long Haul Fitness Exercise Directory</h1>
-<input bind:value={query} type="text" placeholder="Search exercises" />
+<input
+	on:keyup={({ target: { value } }) => debounce(value)}
+	type="text"
+	placeholder="Search exercises"
+/>
 {#if filteredExercises.length > 0}
 	<table>
+		<colgroup>
+			<col style:width="50%" />
+			<col style:width="50%" />
+		</colgroup>
 		<thead>
 			<tr>
 				<th>Exercise</th>
@@ -39,8 +58,8 @@
 			</tr>
 		</thead>
 		<tbody>
-			{#each filteredExercises as exercise}
-				<tr>
+			{#each filteredExercises as exercise (exercise.pk)}
+				<tr in:fade|local out:fly|local={{ x: 100, duration: 200 }}>
 					<td><a href="{base}/exercises/{exercise.pk}">{exercise.fields.name}</a></td>
 					<td>
 						<ul>
@@ -58,6 +77,10 @@
 {/if}
 
 <style>
+	table {
+		table-layout: fixed;
+	}
+
 	tbody tr:nth-child(even) {
 		background-color: #f6f8fa;
 	}
