06c14b399d936ddc79243bfe81b99938fa8e5620
Bill Amberg <wamberg@longhaul.fitness>
2023-05-17 16:13:57 -0400

    Filter exercises by primary muscle

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,15 +6,23 @@
 	export let data;
 	const muscleMap = data.muscles;
 	let query = '';
-	let timer;
+	let muscleFilters = [];
 
 	const handleClickClear = () => {
 		query = '';
+		muscleFilters = [];
 	};
 
 	$: filteredExercises = data.exercises
 		.filter((e) => {
 			return e.fields.name.toLowerCase().match(query.toLowerCase());
+		})
+		.filter((e) => {
+			if (muscleFilters.length === 0) return true;
+			const intersection = e.fields.primary_muscles.filter((value) =>
+				muscleFilters.includes(value.toString())
+			);
+			return intersection.length > 0;
 		})
 		.sort((a, b) => {
 			const nameA = a.fields.name.toUpperCase();
@@ -37,38 +45,50 @@
 </svelte:head>
 
 <h1>Long Haul Fitness Exercise Directory</h1>
-<input bind:value={query} type="text" placeholder="Search exercises" />
-<button on:click={handleClickClear}>Clear Search</button>
-{#if filteredExercises.length > 0}
-	<table>
-		<colgroup>
-			<col style:width="50%" />
-			<col style:width="50%" />
-		</colgroup>
-		<thead>
-			<tr>
-				<th>Exercise</th>
-				<th>Primary Muscles</th>
-			</tr>
-		</thead>
-		<tbody>
-			{#each filteredExercises as exercise (exercise.pk)}
-				<tr in:fade|local out:fly|local={{ x: 100, duration: 200 }}>
-					<td><a href="{base}/exercises/{exercise.pk}">{exercise.fields.name}</a></td>
-					<td>
-						<ul>
-							{#each translateMuscleIdsToNames(exercise.fields.primary_muscles, muscleMap) as muscle}
-								<li>{muscle}</li>
-							{/each}
-						</ul>
-					</td>
-				</tr>
-			{/each}
-		</tbody>
-	</table>
-{:else}
-	<p>No exercises match query <strong>{query}</strong></p>
-{/if}
+<div class="u-grid">
+	<div class="exercise-filters">
+		<input bind:value={query} type="text" placeholder="Search exercises" />
+		<button on:click={handleClickClear}>Clear Search</button>
+		{#each Object.entries(muscleMap) as [id, name]}
+			<label>
+				<input type="checkbox" bind:group={muscleFilters} name="muscleFilters" value={id} />
+				{name}
+			</label>
+		{/each}
+	</div>
+	<div class="exercise-list">
+		{#if filteredExercises.length > 0}
+			<table>
+				<colgroup>
+					<col style:width="50%" />
+					<col style:width="50%" />
+				</colgroup>
+				<thead>
+					<tr>
+						<th>Exercise</th>
+						<th>Primary Muscles</th>
+					</tr>
+				</thead>
+				<tbody>
+					{#each filteredExercises as exercise (exercise.pk)}
+						<tr in:fade|local out:fly|local={{ x: 100, duration: 200 }}>
+							<td><a href="{base}/exercises/{exercise.pk}">{exercise.fields.name}</a></td>
+							<td>
+								<ul>
+									{#each translateMuscleIdsToNames(exercise.fields.primary_muscles, muscleMap) as muscle}
+										<li>{muscle}</li>
+									{/each}
+								</ul>
+							</td>
+						</tr>
+					{/each}
+				</tbody>
+			</table>
+		{:else}
+			<p>No exercises match query <strong>{query}</strong></p>
+		{/if}
+	</div>
+</div>
 
 <style>
 	table {
